8d4b6a9598b957a4e85a12ab97458d84bb582e9c
Marc Tiehuis <marctiehuis@gmail.com>
2018-08-01 21:28:52 +1200

    Add f32 float printing code

---
+++ README.md
@@ -0,0 +1,10 @@
+Conversion of https://github.com/ulfjack/ryu to [zig](https://ziglang.org/).
+
+ - [ ] Review all manual casts
+ - [ ] Make more idiomatic
+ - [ ] Add f16 variant
+ - [ ] Add f128 variant (Use partial table set)
+ - [ ] Add specified precision argument (current errol does this after but this is
+   slows things down a fair bit).
+ - [ ] Benchmark against current float printing code (memory
+   consumption/performance).
---
+++ common.zig
@@ -0,0 +1,63 @@
+// Copyright 2018 Ulf Adams
+//
+// The contents of this file may be used under the terms of the Apache License,
+// Version 2.0.
+//
+//    (See accompanying file LICENSE-Apache or copy at
+//     http://www.apache.org/licenses/LICENSE-2.0)
+//
+// Alternatively, the contents of this file may be used under the terms of
+// the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE-Boost or copy at
+//     https://www.boost.org/LICENSE_1_0.txt)
+//
+// Unless required by applicable law or agreed to in writing, this software
+// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.
+
+const std = @import("std");
+
+// Returns e == 0 ? 1 : ceil(log_2(5^e)).
+pub inline fn pow5bits(e: i32) u32 {
+    // This approximation works up to the point that the multiplication overflows at e = 3529.
+    // If the multiplication were done in 64 bits, it would fail at 5^4004 which is just greater
+    // than 2^9297.
+    std.debug.assert(e >= 0);
+    std.debug.assert(e <= 3528);
+    return ((@intCast(u32, e) * 1217359) >> 19) + 1;
+}
+
+// Returns floor(log_10(2^e)).
+pub inline fn log10Pow2(e: i32) i32 {
+    // The first value this approximation fails for is 2^1651 which is just greater than 10^297.
+    std.debug.assert(e >= 0);
+    std.debug.assert(e <= 1650);
+    return @intCast(i32, (@intCast(u32, e) * 78913) >> 18);
+}
+
+// Returns floor(log_10(5^e)).
+pub inline fn log10Pow5(e: i32) i32 {
+    // The first value this approximation fails for is 5^2621 which is just greater than 10^1832.
+    std.debug.assert(e >= 0);
+    std.debug.assert(e <= 2620);
+    return @intCast(i32, (@intCast(u32, e) * 732923) >> 20);
+}
+
+pub inline fn copy_special_str(result: []u8, sign: bool, exponent: bool, mantissa: bool) usize {
+    if (mantissa) {
+        std.mem.copy(u8, result, "nan");
+        return 3;
+    }
+    if (sign) {
+        result[0] = '-';
+    }
+
+    const offset: usize = @boolToInt(sign);
+
+    if (exponent) {
+        std.mem.copy(u8, result[offset..], "inf");
+        return offset + 3;
+    }
+    std.mem.copy(u8, result[offset..], "0E0");
+    return offset + 3;
+}
